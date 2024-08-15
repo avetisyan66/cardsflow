4434fa7065765c6a17b273ec9d34085a0410307e
Ani <avetisyanani6@gmail.com>
2024-08-15 12:15:36 +0400

    mini change

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Provider } from "react-redux";
 import store from "./application/store";
 
 import Home from "./presentation/screens/home/Home";
-
+ 
 function App() {
   return (
     <Provider store={store}>
